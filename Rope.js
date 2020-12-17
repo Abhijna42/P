4a9f1d4274d5c9a0dca7b75726f71689d02f616b
Abhijna42 <74900832+Abhijna42@users.noreply.github.com>
2020-12-17 11:03:27 +0530

    Add files via upload

---
+++ Rope.js
@@ -0,0 +1,20 @@
+class Rope{
+    constructor(body1,body2,xOffset){
+var options={
+        bodyA:body1,
+bodyB:body2,
+pointB:{x:xOffset,y:0}
+}
+    this.rope = Constraint.create(options);
+    this.xOffset = xOffset;
+    World.add(world,this.rope);
+    }
+    display(){
+        var pointA=this.rope.bodyA.position;
+        var pointB=this.rope.bodyB.position;
+        push();
+        strokeWeight(3);
+        stroke("white");
+        line(pointA.x,pointA.y,pointB+this.xOffset,pointB.y);
+    }
+}
